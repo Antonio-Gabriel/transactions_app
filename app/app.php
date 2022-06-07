0f6af338ae1d199499871a1171ac08be40623bf6
António Gabriel <antoniocamposgabriel@gmail.com>
2022-06-07 11:37:07 +0100

    calculate total income, expense and nettotal bussiness logic

--- app/app.php
+++ app/app.php
@@ -66,3 +66,20 @@ function extractTransaction(array $transactionRow): array
         "amount" => $amount
     ];
 }
+
+function calculateTotals(array $transactions): array
+{
+    $totals = ["netTotal" => 0, "totalIncome" => 0, "totalExpenses" => 0];
+
+    foreach ($transactions as $transaction) {
+        $totals["netTotal"] += $transaction["amount"];
+
+        if ($transaction["amount"] >= 0) {
+            $totals["totalIncome"] += $transaction["amount"];
+        } else {
+            $totals["totalExpense"] += $transaction["amount"];
+        }
+    }
+
+    return $totals;
+}
