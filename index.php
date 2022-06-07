ec3ddb2b0ebe9e5ec5282cb0df8725112ff474d0
António Gabriel <antoniocamposgabriel@gmail.com>
2022-06-07 09:40:29 +0100

    configure path dirs

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
+$root = __DIR__ . DIRECTORY_SEPARATOR;
+
+define("APP_PATH", $root . "app" . DIRECTORY_SEPARATOR);
+define("VIEWS_PATH", $root . "views" . DIRECTORY_SEPARATOR);
+define("FILE_PATH", $root . "transactionFile" . DIRECTORY_SEPARATOR);
+
+print($root);
