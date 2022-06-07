e0f28721966768c17f401be9b9f9ace05ab2ece4
António Gabriel <antoniocamposgabriel@gmail.com>
2022-06-07 10:31:51 +0100

    extract transactions into file

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $root = __DIR__ . DIRECTORY_SEPARATOR;
 
 define("APP_PATH", $root . "app" . DIRECTORY_SEPARATOR);
 define("VIEWS_PATH", $root . "views" . DIRECTORY_SEPARATOR);
+define("HELPERS_PATH", $root . "helpers" . DIRECTORY_SEPARATOR);
 define("FILE_PATH", $root . "transactionFile" . DIRECTORY_SEPARATOR);
 
 /**
@@ -13,6 +14,15 @@ define("FILE_PATH", $root . "transactionFile" . DIRECTORY_SEPARATOR);
  */
 
 require APP_PATH . "app.php";
+require HELPERS_PATH . "prettyPrintArray.php";
 
 $files = getTransactionsFile(FILE_PATH);
-var_dump($files);
+
+$transactions = [];
+foreach ($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file));
+}
+
+prettyPrintArray(
+    $transactions
+);
