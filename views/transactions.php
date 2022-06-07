1de45455bbc6dfcfbe0815c2f8e119714344411e
António Gabriel <antoniocamposgabriel@gmail.com>
2022-06-07 10:41:13 +0100

    show transactions data into view page

--- views/transactions.php
+++ views/transactions.php
@@ -43,7 +43,17 @@
         </thead>
 
         <tbody>
+            <?php if (!empty($transactions)) : ?>
+                <?php foreach ($transactions as $transaction) : ?>
+                    <tr>
+                        <td><?= $transaction[0] ?></td>
+                        <td><?= $transaction[1] ?></td>
+                        <td><?= $transaction[2] ?></td>
+                        <td><?= $transaction[3] ?></td>
+                    </tr>
+                <?php endforeach; ?>
 
+            <?php endif; ?>
         </tbody>
 
         <tfoot>
