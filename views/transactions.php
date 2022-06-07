1561e8beb2e3fe0bb41ca4e39bc33899ebe77e0a
António Gabriel <antoniocamposgabriel@gmail.com>
2022-06-07 11:48:54 +0100

    format dollar amount on view

--- views/transactions.php
+++ views/transactions.php
@@ -46,10 +46,10 @@
             <?php if (!empty($transactions)) : ?>
                 <?php foreach ($transactions as $transaction) : ?>
                     <tr>
-                        <td><?= $transaction["date"] ?></td>
-                        <td><?= $transaction["checkNumber"] ?></td>
-                        <td><?= $transaction["description"] ?></td>
-                        <td><?= $transaction["amount"] ?></td>
+                        <td><?= $transaction["date"]; ?></td>
+                        <td><?= $transaction["checkNumber"]; ?></td>
+                        <td><?= $transaction["description"]; ?></td>
+                        <td><?= formatDollarAmount($transaction["amount"]); ?></td>
                     </tr>
                 <?php endforeach; ?>
 
@@ -59,17 +59,17 @@
         <tfoot>
             <tr>
                 <th colspan="3">Total Income:</th>
-                <td><?= $totals["totalIncome"] ?? 0; ?></td>
+                <td><?= formatDollarAmount($totals["totalIncome"] ?? 0); ?></td>
             </tr>
 
             <tr>
                 <th colspan="3">Total Expense:</th>
-                <td><?= $totals["totalExpense"] ?? 0; ?></td>
+                <td><?= formatDollarAmount($totals["totalExpense"] ?? 0); ?></td>
             </tr>
 
             <tr>
                 <th colspan="3">Net Total:</th>
-                <td><?= $totals["netTotal"] ?? 0; ?></td>
+                <td><?= formatDollarAmount($totals["netTotal"] ?? 0); ?></td>
             </tr>
         </tfoot>
     </table>
