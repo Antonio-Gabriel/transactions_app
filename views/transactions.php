31e7297851718152a99aea0e5a94c1c5bb344f0d
António Gabriel <antoniocamposgabriel@gmail.com>
2022-06-07 11:38:18 +0100

    add calculate totals results on view front

--- views/transactions.php
+++ views/transactions.php
@@ -59,17 +59,17 @@
         <tfoot>
             <tr>
                 <th colspan="3">Total Income:</th>
-                <td></td>
+                <td><?= $totals["totalIncome"] ?? 0; ?></td>
             </tr>
 
             <tr>
                 <th colspan="3">Total Expense:</th>
-                <td></td>
+                <td><?= $totals["totalExpense"] ?? 0; ?></td>
             </tr>
 
             <tr>
                 <th colspan="3">Net Total:</th>
-                <td></td>
+                <td><?= $totals["netTotal"] ?? 0; ?></td>
             </tr>
         </tfoot>
     </table>
